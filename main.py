6b1894da619a371d2f378a4eb8676cac21f90dad
liuhaozan <1712450671@qq.com>
2026-02-09 21:21:30 +0800

    修改了main

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import torch
+def hello():
+    print("Hello, World!1111")
+
 
 if __name__ == "__main__":
+    hello()
     print("Hello, World!")
     print(f"PyTorch version: {torch.__version__}")
